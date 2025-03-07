6aedae11e21edadfbf08a9e4c5e3f35056f45bab
Upender Mallepally <upendermallepally@upenders-mbp.home>
2025-03-07 18:09:12 -0500

    Fix start command

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id, "name": "Sample Item"}
